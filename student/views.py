c41be4e680b8e4b95191920543fc88a55869297c
teamKweku <teamkweku@outlook.com>
2022-06-29 14:51:21 -0500

    Building the student model form and settings up boostrap theme

--- student/views.py
+++ student/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Student
 
 # Create your views here.
 def index(request):
+    students = Student.objects.all()
     context = {
-        
+        'students': students,
     }
     return render(request, 'students/index.html', context)
